e4600ff0a23e9484ec7fc10bafaf0982b34666dc
MuhammadZaid1999 <57443175+MuhammadZaid1999@users.noreply.github.com>
2024-04-30 16:35:09 +0500

    creating frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hello World
+      </h1>
+      <Navbar/>
+    </>
+  );
+}
+
+export default App;

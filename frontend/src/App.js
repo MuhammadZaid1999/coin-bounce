28943064f24a41d0ff689d4ff731b9bd051407cb
MuhammadZaid1999 <57443175+MuhammadZaid1999@users.noreply.github.com>
2024-05-02 22:38:00 +0500

    login design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import styles from "./App.module.css";
 import Protected from "./components/Protected/Protected";
 import Error from "./pages/Error/Error";
+import Login from "./pages/Login/Login";
 
 function App() {
   const isAuth = true;
@@ -54,7 +55,7 @@ function App() {
             <Route
               path = 'login'
               exact
-              element={<div className={styles.main}>Log in Page</div>}
+              element={<div className={styles.main}><Login/></div>}
             />
 
             <Route

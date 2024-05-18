acdea97ae2caa88ece66e4a8e0a9d42b0d5fa09b
MuhammadZaid1999 <57443175+MuhammadZaid1999@users.noreply.github.com>
2024-05-18 17:24:17 +0500

    complete application

--- frontend/src/api/external.js
+++ frontend/src/api/external.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
-const NEWS_API_ENDPOINT = `https://newsapi.org/v2/everything?q=business AND 
-blockchain&sortBy=publishedAt&language=en&apiKey=${NEWS_API_KEY}`;
+// const NEWS_API_ENDPOINT = `https://newsapi.org/v2/everything?q=business AND 
+// blockchain&sortBy=publishedAt&language=en&apiKey=${NEWS_API_KEY}`;
+
+const NEWS_API_ENDPOINT =
+  "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json";
 
 const CRYPTO_API_ENDPOINT = `https://api.coingecko.com/api/v3/coins/markets?
 vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
